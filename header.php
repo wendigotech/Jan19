bf380387399444c6e1c775526c3ad006a5ed16bb
MB <wendigotech@gmail.com>
2019-01-19 01:38:56 +0100

    1st

---
+++ header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta class="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <!-- Don't forget to add your metadata here -->
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+        <?php wp_head(); ?>
+    </head>
+    <body class="<?php echo implode(' ', get_body_class()); ?>">
