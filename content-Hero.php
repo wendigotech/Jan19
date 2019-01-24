a5837d8c8f5b10f332e13ba732adeace8b12f6a3
MB <wendigotech@gmail.com>
2019-01-25 00:58:47 +0100

    add dropdown menu hover & item styles html content in hero content

--- content-Hero.php
+++ content-Hero.php
@@ -6,8 +6,8 @@
                     <div class="hero__content">
                         <div class="hero__content__inner" id='navConverter'>
                             <h1 class="hero__title"><?php _e( 'A production-ready theme for your projects', 'jan19' ); ?></h1>
-                            <?php the_content(); ?>
-                            <?php the_content(); ?>
+                            <p class="hero__text"><?php _e( 'Evie is an MIT licensed template bundled with a minimal style guide to build websites faster. It is extemely lightweight, customizable and works perfectly on modern browsers.', 'jan19' ); ?></p>
+                            <p class="hero__text"><?php _e( 'Evie is an MIT licensed template bundled with a minimal style guide to build websites faster. It is extemely lightweight, customizable and works perfectly on modern browsers.', 'jan19' ); ?></p>
                             <a href="#" class="button button__accent"><?php _e( 'Download Evie', 'jan19' ); ?></a>
                             <a href="#" class="button hero__button"><?php _e( 'Learn more', 'jan19' ); ?></a>
                         </div>
