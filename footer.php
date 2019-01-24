3741000d1a933eb9b540f2e5a95d0f5a5778178c
MB <wendigotech@gmail.com>
2019-01-24 23:59:51 +0100

    2

--- footer.php
+++ footer.php
@@ -1,2 +1,51 @@
-<?php wp_footer(); ?></body>
+</div>
+        <!-- Steps -->
+        <!-- Expanded sections -->
+        <!-- Call To Action -->
+        <!-- Footer -->
+        <div class="footer footer--dark">
+            <div class="container">
+                <div class="footer__inner">
+                    <a href="index.html" class="footer__textLogo"><?php _e( 'Evie theme', 'jan19' ); ?></a>
+                    <div class="footer__data">
+                        <div class="footer__data__item">
+                            <div class="footer__row">
+                                <?php _e( 'Created by', 'jan19' ); ?> 
+                                <a href="https://twitter.com/ninalimpi" target="_blank" class="footer__link"><?php _e( 'Katerina Limpitsouni', 'jan19' ); ?></a>
+                            </div>
+                            <div class="footer__row">
+                                <?php _e( 'Code/design by', 'jan19' ); ?> 
+                                <a href="https://twitter.com/anges244" target="_blank" class="footer__link"><?php _e( 'Aggelos Gesoulis', 'jan19' ); ?></a>
+                            </div>
+                        </div>
+                        <div class="footer__data__item">
+                            <div class="footer__row"><?php _e( 'Created for', 'jan19' ); ?> 
+                                <a href="https://undraw.co" target="_blank" class="footer__link"><?php _e( 'unDraw', 'jan19' ); ?></a>
+                            </div>
+                            <div class="footer__row"><?php _e( 'Special thx to', 'jan19' ); ?> 
+                                <a href="https://shareboost.co" target="_blank" class="footer__link"><?php _e( 'ShareBoost', 'jan19' ); ?></a> <?php _e( 'for the support', 'jan19' ); ?>
+                            </div>
+                        </div>
+                        <div class="footer__data__item">
+                            <div class="footer__row">
+                                <a href="https://github.com/anges244/evie" target="_blank" class="footer__link"><?php _e( 'GitHub', 'jan19' ); ?></a>
+                            </div>
+                            <div class="footer__row">
+                                <a href="https://twitter.com/undraw_co" target="_blank" class="footer__link"><?php _e( 'Twitter', 'jan19' ); ?></a>
+                            </div>
+                            <div class="footer__row">
+                                <a href="https://www.facebook.com/undraw.co/" target="_blank" class="footer__link"><?php _e( 'Facebook', 'jan19' ); ?></a>
+                            </div>
+                            <div class="footer__row">
+                                <a href="./additional.html" class="footer__link"><?php _e( 'MIT license', 'jan19' ); ?></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            
+            
+            
+    <?php wp_footer(); ?></body>
 </html>
