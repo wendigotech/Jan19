b5c4ba9313bfe71222ef411b2e0c8b41908be02b
MB <wendigotech@gmail.com>
2019-01-25 00:27:48 +0100

    add hero template part

---
+++ content-Hero.php
@@ -0,0 +1,17 @@
+<div class="hero">
+            <div class="hero__overlay hero__overlay--gradient"></div>
+            <div class="hero__mask"></div>
+            <div class="hero__inner">
+                <div class="container">
+                    <div class="hero__content">
+                        <div class="hero__content__inner" id='navConverter'>
+                            <h1 class="hero__title"><?php _e( 'A production-ready theme for your projects', 'jan19' ); ?></h1>
+                            <?php the_content(); ?>
+                            <?php the_content(); ?>
+                            <a href="#" class="button button__accent"><?php _e( 'Download Evie', 'jan19' ); ?></a>
+                            <a href="#" class="button hero__button"><?php _e( 'Learn more', 'jan19' ); ?></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
