cb5959476016335db28c7f604b60ccead42317bb
MB <wendigotech@gmail.com>
2019-01-25 01:50:32 +0100

    8

--- page-hero.php
+++ page-hero.php
@@ -1,27 +1,5 @@
 <?php
-/*
- Template Name: hero
- Template Post Type: post, page
-*/
-?>
-<?php
 get_header(); ?>
-<div class="hero">
-            <div class="hero__overlay hero__overlay--gradient"></div>
-            <div class="hero__mask"></div>
-            <div class="hero__inner">
-                <div class="container">
-                    <div class="hero__content">
-                        <div class="hero__content__inner" id='navConverter'>
-                            <h1 class="hero__title"><?php _e( 'A production-ready theme for your projects', 'jan19' ); ?></h1>
-                            <p class="hero__text"><?php _e( 'Evie is an MIT licensed template bundled with a minimal style guide to build websites faster. It is extemely lightweight, customizable and works perfectly on modern browsers.', 'jan19' ); ?></p>
-                            <p class="hero__text"><?php _e( 'Evie is an MIT licensed template bundled with a minimal style guide to build websites faster. It is extemely lightweight, customizable and works perfectly on modern browsers.', 'jan19' ); ?></p>
-                            <a href="#" class="button button__accent"><?php _e( 'Download Evie', 'jan19' ); ?></a>
-                            <a href="#" class="button hero__button"><?php _e( 'Learn more', 'jan19' ); ?></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+
 
 <?php get_footer(); ?>
