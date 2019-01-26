b83657425ba9a9715a7c35e3ffe32720e847f088
MB <wendigotech@gmail.com>
2019-01-26 21:58:03 +0100

    2

--- content-Hero.php
+++ content-Hero.php
@@ -4,12 +4,8 @@
             <div class="hero__inner">
                 <div class="container">
                     <div class="hero__content">
-                        <?php
-                    $hero_args = array(
-                        'category_name' => 'hero'
-                    )
-                ?><?php $hero = new WP_Query( $hero_args ); ?><?php if ( $hero->have_posts() ) : ?><?php $hero_item_number = 0; ?><?php while ( $hero->have_posts() && $hero_item_number++ < 1 ) : $hero->the_post(); ?><div class="hero__content__inner" id='navConverter'>
-                            <div class="blog-slider">
+                        <div class="hero__content__inner" id='navConverter'>
+                            <h1 class="hero__title"><?php the_title(); ?></h1><div class="blog-slider">
                                 <div class="blog-slider__wrp swiper-wrapper">
                                     <div class="blog-slider__item swiper-slide">
                                         <div class="blog-slider__img">
@@ -46,12 +42,12 @@
                                     </div>
                                 </div>
                                 <div class="blog-slider__pagination"></div>
-                            </div><h1 class="hero__title"><?php the_title(); ?></h1>
+                            </div>
                             <p class="hero__text"><?php the_content(); ?></p>
                             
                             <a href="#" class="button button__accent"><?php _e( 'Download Evie', 'jan19' ); ?></a>
                             <a href="#" class="button hero__button"><?php _e( 'Learn more', 'jan19' ); ?></a>
-                        </div><?php endwhile; ?><?php wp_reset_postdata(); ?><?php else : ?><p><?php _e( 'Sorry, no posts matched your criteria.', 'jan19' ); ?></p><?php endif; ?>
+                        </div>
                     </div>
                 </div>
             </div>
