c78dd010844f1c7d0188d45ed332e02515c4df47
MB <wendigotech@gmail.com>
2019-01-28 02:55:24 +0100

    7

--- content-Hero.php
+++ content-Hero.php
@@ -21,28 +21,8 @@
                                         </div>
                                         <div class="blog-slider__content"><?php the_content(); ?></div>
                                     </div>
-                                    <div class="blog-slider__item swiper-slide">
-                                        <div class="blog-slider__img">
-                                            <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759871/jason-leung-798979-unsplash.jpg" alt="">
-                                        </div>
-                                        <div class="blog-slider__content">
-                                            <span class="blog-slider__code"><?php _e( '26 December 2019', 'jan19' ); ?></span>
-                                            <div class="blog-slider__title"><?php _e( 'Lorem Ipsum Dolor2', 'jan19' ); ?></div>
-                                            <div class="blog-slider__text"><?php _e( 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?', 'jan19' ); ?></div>
-                                            <a href="#" class="blog-slider__button"><?php _e( 'READ MORE', 'jan19' ); ?></a>
-                                        </div>
-                                    </div>
-                                    <div class="blog-slider__item swiper-slide">
-                                        <div class="blog-slider__img">
-                                            <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759871/alessandro-capuzzi-799180-unsplash.jpg" alt="">
-                                        </div>
-                                        <div class="blog-slider__content">
-                                            <span class="blog-slider__code"><?php _e( '26 December 2019', 'jan19' ); ?></span>
-                                            <div class="blog-slider__title"><?php _e( 'Lorem Ipsum Dolor', 'jan19' ); ?></div>
-                                            <div class="blog-slider__text"><?php _e( 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?', 'jan19' ); ?></div>
-                                            <a href="#" class="blog-slider__button"><?php _e( 'Full review', 'jan19' ); ?></a>
-                                        </div>
-                                    </div>
+                                    
+                                    
                                 </div><?php endwhile; ?><?php wp_reset_postdata(); ?><?php else : ?><p><?php _e( 'Sorry, no posts matched your criteria.', 'jan19' ); ?></p><?php endif; ?>
                                 <div class="blog-slider__pagination"></div>
                             </div>
