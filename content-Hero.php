fe2954a62ad5aa567ec1d1c40145dfedcf3487e0
MB <wendigotech@gmail.com>
2019-01-28 02:47:14 +0100

    4

--- content-Hero.php
+++ content-Hero.php
@@ -2,7 +2,7 @@
             <div class="hero__overlay hero__overlay--gradient"></div>
             <div class="hero__mask"></div>
             <div class="hero__inner">
-                <div class="container-fluid">
+                <div class="container">
                     <div class="hero__content">
                         <div class="hero__content__inner" id='navConverter'>
                             <h1 class="hero__title"><?php _e( 'A production-ready theme for your projects', 'jan19' ); ?></h1><div class="blog-slider">
@@ -11,12 +11,7 @@
                                         <div class="blog-slider__img">
                                             <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759872/kuldar-kalvik-799168-unsplash.jpg" alt="">
                                         </div>
-                                        <div class="blog-slider__content">
-                                            <span class="blog-slider__code"><?php _e( '26 December 2019', 'jan19' ); ?></span>
-                                            <div class="blog-slider__title"><?php _e( 'Lorem Ipsum Dolor', 'jan19' ); ?></div>
-                                            <div class="blog-slider__text"><?php _e( 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?', 'jan19' ); ?> </div>
-                                            <a href="#" class="blog-slider__button"><?php _e( 'READ MORE', 'jan19' ); ?></a>
-                                        </div>
+                                        <div class="blog-slider__content"><?php the_content(); ?></div>
                                     </div>
                                     <div class="blog-slider__item swiper-slide">
                                         <div class="blog-slider__img">
@@ -37,7 +32,7 @@
                                             <span class="blog-slider__code"><?php _e( '26 December 2019', 'jan19' ); ?></span>
                                             <div class="blog-slider__title"><?php _e( 'Lorem Ipsum Dolor', 'jan19' ); ?></div>
                                             <div class="blog-slider__text"><?php _e( 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?', 'jan19' ); ?></div>
-                                            <a href="#" class="blog-slider__button"><?php _e( 'Full review', 'jan19' ); ?></a><a href="#" class="blog-slider__button"><?php _e( 'Claim Bonus', 'jan19' ); ?></a><a href="#" class="blog-slider__button" style="background-image: linear-gradient(90deg, rgba(0, 0, 0, 0.18) 69.863%, rgba(0, 0, 0, 0.18) 69.863%);"><?php _e( 't&C Apply', 'jan19' ); ?></a>
+                                            <a href="#" class="blog-slider__button"><?php _e( 'Full review', 'jan19' ); ?></a>
                                         </div>
                                     </div>
                                 </div>
