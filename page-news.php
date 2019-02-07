72b410a6f21004db613af86e3f7ac421cc3a378b
MB <wendigotech@gmail.com>
2019-02-07 12:03:03 +0100

    2

--- page-news.php
+++ page-news.php
@@ -1,10 +1,10 @@
 <?php
 get_header(); ?>
 
-<div class="pg-empty-placeholder"></div><div class="steps landing__section">
+<div class="pg-empty-placeholder"></div><?php if ( have_posts() ) : ?><?php while ( have_posts() ) : the_post(); ?><div <?php post_class( 'steps landing__section' ); ?> id="post-<?php the_ID(); ?>">
 
         <div class="container"><?php the_content(); ?></div>
-    </div>
+    </div><?php endwhile; ?><?php else : ?><p><?php _e( 'Sorry, no posts matched your criteria.', 'jan19' ); ?></p><?php endif; ?>
 
 
 
