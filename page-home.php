9bcc25f471cd7b0085e33ae0ce0d23a071cf2005
MB <wendigotech@gmail.com>
2019-02-07 19:22:34 +0100

    2

--- page-home.php
+++ page-home.php
@@ -5,19 +5,7 @@ get_header(); ?>
         <span id="scrollToNext" class="scroll"> <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" class='hero__sub__down' fill="currentColor" width="512px" height="512px" viewbox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
                 <path d="M256,298.3L256,298.3L256,298.3l174.2-167.2c4.3-4.2,11.4-4.1,15.8,0.2l30.6,29.9c4.4,4.3,4.5,11.3,0.2,15.5L264.1,380.9c-2.2,2.2-5.2,3.2-8.1,3c-3,0.1-5.9-0.9-8.1-3L35.2,176.7c-4.3-4.2-4.2-11.2,0.2-15.5L66,131.3c4.4-4.3,11.5-4.4,15.8-0.2L256,298.3z"/>
             </svg> </span>
-    </div><?php get_template_part( 'content', 'news' ); ?><div class="expanded landing__section">
-        <div class="container">
-            <div class="expanded__inner">
-                <div class="expanded__media">
-                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/./images/undraw_browser.svg" class="expanded__image">
-                </div>
-                <div class="expanded__content">
-                    <h2 class="expanded__title"><?php _e( 'Natural styling with almost nothing to learn', 'jan19' ); ?></h2>
-                    <p class="expanded__text"><?php _e( 'Evie brings you the pages you\'ll need and the structure to create your own, without a learning curve. It is minimal and mostly styles plain elements. There are only a few classes you\'ll need to know but nothing to disrupt your preferred coding style.', 'jan19' ); ?></p>
-                </div>
-            </div>
-        </div>
-    </div>
+    </div><div class="steps landing__section"><?php the_content(); ?></div><div class="expanded landing__section"><?php get_template_part( 'content', 'news' ); ?></div>
 
 
 
