ffd54bae11204834fb9a04f9233106c7818470cf
MB <wendigotech@gmail.com>
2019-02-12 21:30:46 +0100

    4

--- content-news.php
+++ content-news.php
@@ -8,17 +8,18 @@
     ?><?php $news = new WP_Query( $news_args ); ?><?php if ( $news->have_posts() ) : ?><div class="container">
                         <div class="steps__inner">
                             <?php $news_item_number = 0; ?><?php while ( $news->have_posts() && $news_item_number++ < 3 ) : $news->the_post(); ?><div class="step">
-                                <div class="step__media">
-                                    <?php if ( is_singular() ) : ?><?php
-                                    if ( has_post_thumbnail() ) {
-                                        the_post_thumbnail( 'normal', array(
-                                        'class' => 'step__image'
-                                    ) );
-                                    }
-                                 ?><?php elseif ( has_post_thumbnail( get_the_ID() ) ) : ?><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_post_thumbnail( 'normal', array(
-                                            'class' => 'step__image'
-                                    ) ); ?></a><?php endif; ?>
-                                </div><?php if ( is_singular() ) : ?><h4 class="font-weight-bold" href="#"><?php the_title(); ?></h4><?php else : ?><h4 class="font-weight-bold" href="#"><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a></h4><?php endif; ?>
+                                <div class="card"> 
+                            <?php
+                                if ( has_post_thumbnail() ) {
+                                    the_post_thumbnail( 'normal', array(
+                                    'class' => 'card-img-top page-link'
+                                ) );
+                                }
+                             ?> 
+                            <div class="card-body"> 
+                                <h4 class="card-title font-weight-bold page-link text-dark"><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a></h4>                                                                                                    
+                            </div>                             
+                        </div>
                                 
                                 
                             </div><?php endwhile; ?><?php wp_reset_postdata(); ?>
