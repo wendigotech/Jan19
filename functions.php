a1007b08051ee5c701da7090160ec4649fb78f8f
MB <wendigotech@gmail.com>
2019-02-12 19:59:03 +0100

    edit casino pages edit home tips section

--- functions.php
+++ functions.php
@@ -130,10 +130,6 @@ function jan19_customize_register( $wp_customize ) {
     $wp_customize->add_section( 'Submenu', array(
         'title' => __( 'Submenu', 'jan19' )
     ));
-
-    $wp_customize->add_section( 'submenu_title', array(
-        'title' => __( 'Submenu Title', 'jan19' )
-    ));
     $pgwp_sanitize = function_exists('pgwp_sanitize_placeholder') ? 'pgwp_sanitize_placeholder' : null;
 
     $wp_customize->add_section( 'footer', array(
@@ -208,18 +204,6 @@ function jan19_customize_register( $wp_customize ) {
         'label' => __( 'Footer Bottom', 'jan19' ),
         'type' => 'text',
         'section' => 'footer'
-    ));
-
-    $wp_customize->add_setting( 'tips_title', array(
-        'type' => 'theme_mod',
-        'default' => __( 'Strategy &amp; Tips', 'jan19' ),
-        'sanitize_callback' => $pgwp_sanitize
-    ));
-
-    $wp_customize->add_control( 'tips_title', array(
-        'label' => __( 'Tips title', 'jan19' ),
-        'type' => 'text',
-        'section' => 'submenu_title'
     ));
 
     $wp_customize->add_setting( 'submenu_text', array(
--- page-editors-choice.php
+++ page-editors-choice.php
@@ -22,7 +22,25 @@ get_header(); ?>
             </svg> </span>
     </div><?php get_template_part( 'content', 'choice' ); ?><?php if ( have_posts() ) : ?><?php while ( have_posts() ) : the_post(); ?><div <?php post_class( 'expanded landing__section' ); ?> id="post-<?php the_ID(); ?>">
         <div class="container"><?php the_content(); ?></div>
-    </div><?php endwhile; ?><?php else : ?><p><?php _e( 'Sorry, no posts matched your criteria.', 'jan19' ); ?></p><?php endif; ?><?php get_template_part( 'content', 'tips' ); ?>
+    </div><?php endwhile; ?><?php else : ?><p><?php _e( 'Sorry, no posts matched your criteria.', 'jan19' ); ?></p><?php endif; ?><div class="expanded landing__section">
+        <h2 class="text-center font-weight-bold text-uppercase"><?php _e( 'Casino Tips', 'jan19' ); ?></h2><?php
+        $editors-tips_args = array(
+            'category_name' => 'strategy',
+            'tag' => 'editors-tips',
+            'order' => 'DESC'
+        )
+    ?><?php $editors-tips = new WP_Query( $editors-tips_args ); ?><?php if ( $editors-tips->have_posts() ) : ?><div class="container">
+            <div class="steps__inner">
+                <?php $editors-tips_item_number = 0; ?><?php while ( $editors-tips->have_posts() && $editors-tips_item_number++ < 3 ) : $editors-tips->the_post(); ?><div class="step">
+                    <div class="step__media"><?php the_content(); ?></div>
+
+
+                </div><?php endwhile; ?><?php wp_reset_postdata(); ?>
+
+
+            </div>
+        </div><?php else : ?><p><?php _e( 'Sorry, no posts matched your criteria.', 'jan19' ); ?></p><?php endif; ?>
+    </div>
 
 
 
