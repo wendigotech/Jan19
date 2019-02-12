5bf625fe5f981f1f7a04dde2276f23254aa0f933
MB <wendigotech@gmail.com>
2019-02-12 21:17:33 +0100

    2

--- content-news.php
+++ content-news.php
@@ -8,23 +8,11 @@
     ?><?php $news = new WP_Query( $news_args ); ?><?php if ( $news->have_posts() ) : ?><div class="container">
                         <div class="steps__inner">
                             <?php $news_item_number = 0; ?><?php while ( $news->have_posts() && $news_item_number++ < 3 ) : $news->the_post(); ?><div class="step">
-                                <?php if ( is_singular() ) : ?><div class="step__media">
-                                    <?php
-                                    if ( has_post_thumbnail() ) {
-                                        the_post_thumbnail( 'normal', array(
-                                        'class' => 'step__image'
-                                    ) );
-                                    }
-                                 ?>
-                                <h4><?php the_title(); ?></h4></div><?php else : ?><div class="step__media"><a href="<?php echo esc_url( get_permalink() ); ?>">
-                                    <?php
-                                        if ( has_post_thumbnail() ) {
-                                            the_post_thumbnail( 'normal', array(
-                                            'class' => 'step__image'
-                                        ) );
-                                        }
-                                     ?>
-                                <h4><?php the_title(); ?></h4></a></div><?php endif; ?>
+                                <?php if ( is_singular() ) : ?><?php $image_attributes = !empty( get_the_ID() ) ? wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'normal' ) : null; ?><div class="step__media" style="<?php if($image_attributes) echo 'background-image:url(\''.$image_attributes[0].'\')' ?>">
+                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/./images/undraw_responsive.svg" class="step__image">
+                                </div><?php else : ?><?php $image_attributes = !empty( get_the_ID() ) ? wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'normal' ) : null; ?><div class="step__media" style="<?php if($image_attributes) echo 'background-image:url(\''.$image_attributes[0].'\')' ?>"><a href="<?php echo esc_url( get_permalink() ); ?>">
+                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/./images/undraw_responsive.svg" class="step__image">
+                                </a></div><?php endif; ?><?php if ( is_singular() ) : ?><h4 class="font-weight-bold"><?php the_title(); ?></h4><?php else : ?><h4 class="font-weight-bold"><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a></h4><?php endif; ?>
                                 
                                 
                             </div><?php endwhile; ?><?php wp_reset_postdata(); ?>
