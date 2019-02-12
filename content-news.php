4229c91ec18a1dcb08dc6055fa2e23ef83dd7f86
MB <wendigotech@gmail.com>
2019-02-12 21:10:59 +0100

    edit news page

--- content-news.php
+++ content-news.php
@@ -8,7 +8,23 @@
     ?><?php $news = new WP_Query( $news_args ); ?><?php if ( $news->have_posts() ) : ?><div class="container">
                         <div class="steps__inner">
                             <?php $news_item_number = 0; ?><?php while ( $news->have_posts() && $news_item_number++ < 3 ) : $news->the_post(); ?><div class="step">
-                                <div class="step__media"><?php the_content(); ?></div>
+                                <?php if ( is_singular() ) : ?><div class="step__media">
+                                    <?php
+                                    if ( has_post_thumbnail() ) {
+                                        the_post_thumbnail( 'normal', array(
+                                        'class' => 'step__image'
+                                    ) );
+                                    }
+                                 ?>
+                                <h4><?php the_title(); ?></h4></div><?php else : ?><div class="step__media"><a href="<?php echo esc_url( get_permalink() ); ?>">
+                                    <?php
+                                        if ( has_post_thumbnail() ) {
+                                            the_post_thumbnail( 'normal', array(
+                                            'class' => 'step__image'
+                                        ) );
+                                        }
+                                     ?>
+                                <h4><?php the_title(); ?></h4></a></div><?php endif; ?>
                                 
                                 
                             </div><?php endwhile; ?><?php wp_reset_postdata(); ?>
