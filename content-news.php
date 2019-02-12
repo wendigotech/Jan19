e70ba4f8764d013bede19d679c61c11d80b1b9d0
MB <wendigotech@gmail.com>
2019-02-12 21:56:35 +0100

    7

--- content-news.php
+++ content-news.php
@@ -9,16 +9,12 @@
                         <div class="steps__inner">
                             <?php $news_item_number = 0; ?><?php while ( $news->have_posts() && $news_item_number++ < 3 ) : $news->the_post(); ?><div class="step">
                                 <div class="step__media">
-                                    <?php
-                                if ( has_post_thumbnail() ) {
-                                    the_post_thumbnail( 'normal', array(
-                                    'class' => 'step__image'
-                                ) );
-                                }
-                             ?>
+                                    <?php if ( has_post_thumbnail( get_the_ID() ) ) : ?><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_post_thumbnail( 'normal', array(
+                                            'class' => 'step__image page-link'
+                                    ) ); ?></a><?php endif; ?>
                                 </div>
-                                <h4><?php the_title(); ?></h4>
-                                <p class="step__text"><?php _e( 'A landing page for your open source projects. Present your solution, easily create docs.', 'jan19' ); ?></p>
+                                <?php if ( is_singular() ) : ?><h4 class="font-weight-bold page-link text-dark"><?php the_title(); ?></h4><?php else : ?><h4 class="font-weight-bold page-link text-dark"><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a></h4><?php endif; ?>
+                                
                             </div><?php endwhile; ?><?php wp_reset_postdata(); ?>
                             
                             
