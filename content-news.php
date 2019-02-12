77a80961fe72269a4d7bf096355296a058ccb767
MB <wendigotech@gmail.com>
2019-02-12 22:10:09 +0100

    9

--- content-news.php
+++ content-news.php
@@ -8,12 +8,8 @@
     ?><?php $news = new WP_Query( $news_args ); ?><?php if ( $news->have_posts() ) : ?><div class="container">
                         <div class="steps__inner">
                             <?php $news_item_number = 0; ?><?php while ( $news->have_posts() && $news_item_number++ < 3 ) : $news->the_post(); ?><div class="step">
-                                <div class="step__media">
-                                    <?php if ( has_post_thumbnail( get_the_ID() ) ) : ?><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_post_thumbnail( 'normal', array(
-                                            'class' => 'step__image'
-                                    ) ); ?></a><?php endif; ?>
-                                </div>
-                                <?php if ( is_singular() ) : ?><h4 class="font-weight-bold text-dark"><?php the_title(); ?></h4><?php else : ?><h4 class="font-weight-bold text-dark"><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a></h4><?php endif; ?>
+                                <div class="step__media"><?php the_content(); ?></div>
+                                
                                 
                             </div><?php endwhile; ?><?php wp_reset_postdata(); ?>
                             
